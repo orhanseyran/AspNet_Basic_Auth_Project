b4e59f3d6172ad9fddb816a398161a50ee3a77e8
orhanseyran <orhanseyran35@gmail.com>
2024-08-17 18:11:06 +0300

    Düzenleme

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -96,5 +96,76 @@ namespace auth.Controllers
                 return View(addUser);
             }
         }
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            ViewBag.User = user;
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var role = await _roleManager.FindByIdAsync(user.Id);
+            ViewBag.RoleId = role;
+      
+            var roles =  _roleManager.Roles.ToList();
+            ViewBag.Role = roles;
+            if (roles == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+        public async Task<IActionResult> EditPost(AddUser addUser, string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Kullanıcı rolünü güncellemek için:
+            var currentRoles = await _userManager.GetRolesAsync(user);
+            var resultRemoveRoles = await _userManager.RemoveFromRolesAsync(user, currentRoles);
+            if (!resultRemoveRoles.Succeeded)
+            {
+                foreach (var error in resultRemoveRoles.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return View(addUser);
+            }
+
+            var resultAddRole = await _userManager.AddToRoleAsync(user, addUser.Role);
+            if (!resultAddRole.Succeeded)
+            {
+                foreach (var error in resultAddRole.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return View(addUser);
+            }
+
+            // Kullanıcı bilgilerini güncelle
+            user.Email = addUser.Email;
+            user.UserName = addUser.Email;
+
+            user.EmailConfirmed = true;
+            user.LockoutEnabled = true;
+
+            var userUpdateResult = await _userManager.UpdateAsync(user);
+            if (userUpdateResult.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in userUpdateResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return View(addUser);
+            }
+        }
+
     }
 }
